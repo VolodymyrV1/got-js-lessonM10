c0f68c65e22b8978c5e9070c53aa386df43dc851
VolodymyrV <kotsiubkov@gmail.com>
2025-09-09 18:40:52 +0300

    1

---
+++ Lesson 19/js/01-timeout.js
@@ -0,0 +1,28 @@
+/*
+ * Метод window.setTimeout(callback, delay, args) 
+ */
+
+
+// console.log("before");
+
+
+// setTimeout(() => {
+//     console.log("timeout-1");
+// }, 0);
+
+// setTimeout(() => {
+//     console.log("timeout-2");
+// }, 0);
+
+
+// console.log("after");
+
+
+
+
+
+/*
+ * Очищення таймаута за допомогою clearTimeout(timeoutId)
+ */
+
+
