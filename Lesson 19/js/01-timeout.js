3e7a4c2386d5c25a486e712cdc4b5d5e4ced2bc2
Volodymyr Kotsiubko <kotsiubkov@gmail.com>
2025-09-10 23:11:46 +0300

    all-lesson

--- Lesson 19/js/01-timeout.js
+++ Lesson 19/js/01-timeout.js
@@ -26,3 +26,25 @@
  */
 
 
+// const logger = (time) => {
+//     console.log(`Timer ${time}`);
+    
+// }
+
+// const timerId = setTimeout(logger, 1500, "2s");
+
+// clearTimeout(timerId);
+
+
+
+
+/*
+ * Можливість передати параметри для колбеку
+ */
+
+// setTimeout(({ name, city }) => {
+//     console.log(`${name} from ${city}`);
+    
+
+// }, 1000, {name: "Petya", city: "Poltava"});
+
