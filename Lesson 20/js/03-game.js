4825f8ae23335e6f6f155a5a33edb8f68f9aadd9
Volodymyr Kotsiubko <kotsiubkov@gmail.com>
2025-09-13 23:27:02 +0300

    mayze

--- Lesson 20/js/03-game.js
+++ Lesson 20/js/03-game.js
@@ -30,6 +30,7 @@ const result = document.querySelector(".result");
 starnBtn.addEventListener("click", handlerStart);
 
 function handlerStart() {
+    result.innerHTML = "";
     const promises = [...container.children].map(() => {
         return new Promise((resolve, reject) => {
             const random = Math.random();
@@ -44,10 +45,21 @@ function handlerStart() {
 
     Promise.allSettled(promises)
         .then(data => {
+            const isWinner = 
+            data.every(item => item.status === "fulfilled") || 
+            data.every(item => item.status === "rejected");
+
             data.forEach((item, i) => {
+                container.children[i].textContent = "";
 
                 setTimeout(() => {
                     container.children[i].textContent = item.value || item.reason;
+                    
+                    if(i === data.length - 1) {
+                        result.textContent = isWinner ? "Winner" : "Loser";
+                    }
+                
+                
                 }, 1000 * (i + 1))
 
                 
